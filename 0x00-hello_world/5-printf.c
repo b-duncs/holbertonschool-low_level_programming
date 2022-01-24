2f3906368982f35b138488c2df5bf75271f5a7d9
b-duncs <baylee.duncan@protonmail.com>
2022-01-24 13:26:52 -0800

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - program startup
+(*
+ * Description: prints the exact phrase followed by a new line)?
+ * Return: return (0) is the required function signature
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
