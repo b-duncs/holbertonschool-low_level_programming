e7802541d73ddb8bff6c95c69ca716a59d7d1fa1
b-duncs <baylee.duncan@protonmail.com>
2022-01-24 13:19:15 -0800

    updated 4-puts.c and obliged betty's demands

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 /**
- *4-puts.c - utilizes the puts command
- *@main: program startup
- (*
- *Description: prints the exact phrase followed by a new line)?
+ * 4-puts.c - utilizes the puts command
+ * main: program startup
+(*
+ * Description: prints the exact phrase followed by a new line)?
+ * Return: return (0) is the required function signature
  */
 
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
