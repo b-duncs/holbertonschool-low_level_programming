777ff0766169acf5ff8cfc9681d45dbe05ff1fb1
b-duncs <baylee.duncan@protonmail.com>
2022-01-24 12:49:58 -0800

    updated 4-puts.c and added comment line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
+/*
+ *4-puts.c
+ *prints exactly '\"Programming is like building a multilingual puzzle' followed by a new line 
+ */
+
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
