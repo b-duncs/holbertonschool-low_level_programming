3b6c5c22edfe8db786f764528de5adf752909d38
b-duncs <baylee.duncan@protonmail.com>
2022-01-24 13:32:16 -0800

    updated 5-printf.c and fixed syntax error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
