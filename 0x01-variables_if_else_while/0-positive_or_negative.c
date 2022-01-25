7df388482180c4d6b28d9708b2b92f65edf6a7ae
b-duncs <baylee.duncan@protonmail.com>
2022-01-25 08:46:27 -0800

    added 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,30 @@
 /**
  * main - program startup
 (*
- * Description: prints the exact phrase followed by a new line)?
+ * Description: assigns a random number to the variable `n` each execution
+ * and prints whether the number stored in `n` is positive or negative)?
  * Return: return (0) is the required function signature
  */
 
 #include <stdio.h>
+#include <time.h>
 int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("is positive");
+	}
+	else if (n == 0)
+	{
+		printf("is zero");
+	}
+	else if (n > 0)
+	{
+		printf("is negative");
+	}
+	return (0);
+}
