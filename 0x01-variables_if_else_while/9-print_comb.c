f26a1b9f9972835fb45e11aeb4c22819c242a1e9
b-duncs <baylee.duncan@protonmail.com>
2022-01-25 13:48:27 -0800

    updated 9-print_comb.c and fixed syntax error

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,7 +13,11 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		putchar((num % 10) + '0');
-		putchar(44);
+		if (num != 9)
+		{
+			putchar(44);
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
