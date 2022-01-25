be0a28d247df034b866f7a0d1fbdbca8d08e2a9d
b-duncs <baylee.duncan@protonmail.com>
2022-01-25 11:10:32 -0800

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/**
+ * main - program startup
+(*
+ * Description: prints the alphabet in lowercase and in reverse)?
+ * Return: return (0) is the required function signature
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar ('\n');
+	return (0);
+}
