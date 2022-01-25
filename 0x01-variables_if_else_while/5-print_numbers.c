2bcfab142bc0ea5e118a56f7fcb866cf766529c9
b-duncs <baylee.duncan@protonmail.com>
2022-01-25 10:42:34 -0800

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * main - program startup
+(*
+ * Description: prints all single digit numbers of base 10 starting from `0`)?
+ * Return: return (0) is the required function signature
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	int base10;;
+
+	for (base10 = '0'; base10 <= '10'; base10++)
+	{
+		printf("%i\n", base10);
+	}
+	return (0);
+}
