72b4ba608773f0616cd7562c974cb22ebe5a2c92
b-duncs <baylee.duncan@protonmail.com>
2022-01-25 09:25:33 -0800

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - program startup
+(*
+ * Description: prints the alphabet in lowercase)?
+ * Return: return (0) is the required function signature
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	return (0);
+}
