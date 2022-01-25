6614b257e9a50982f8fe35e47dfcae7b28acb493
b-duncs <baylee.duncan@protonmail.com>
2022-01-25 08:59:19 -0800

    updated 0-positive_or_negative and fixed operand error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+	if (n > 0)
 	{
 		printf("is positive");
 	}
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("is zero");
 	}
-	else if (n > 0)
+	else if (n < 0)
 	{
 		printf("is negative");
 	}
