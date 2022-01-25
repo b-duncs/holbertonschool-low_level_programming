9e254229cef7edbe0f058072f3637eb2f3a44936
b-duncs <baylee.duncan@protonmail.com>
2022-01-25 13:34:35 -0800

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+/**
+ * main - program startup
+(*
+ * Description: prints all possible combinations of single-digit numbers)?
+ * Return: return (0) is the required function signature
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + ',');
+	}
+	putchar('\n');
+	return (0);
+}
