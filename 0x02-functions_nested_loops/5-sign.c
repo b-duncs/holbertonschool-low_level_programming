cd6bd65d4a53cb68f856ab39501a7c2f33c8dbb1
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 11:20:31 -0800

    updated 5-signs.c and fixed typo

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@
 #include "main.h"
 int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		putchar('+');
 		return (1);
 	}
-	else if (c == 0)
+	else if (n == 0)
 	{
 		putchar('0');
 		return (0);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
 		putchar('-');
 		return (-1);
