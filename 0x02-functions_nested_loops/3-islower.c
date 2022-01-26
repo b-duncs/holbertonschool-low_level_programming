3ec06136159651d38cee670a5fa7f18d6cfc88d4
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 11:04:12 -0800

    updated 3-islower.c and fixed typo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  * @c: int being evaluated
 (*
  * Description: checks for a lowercase character
- * and returns `1` if `c` is lowecase
+ * and returns `1` if `c` is lowercase
  * and returns `0` otherwise)?
  * Return: return (0) exits program successfully, return (1) throws an error
  */
