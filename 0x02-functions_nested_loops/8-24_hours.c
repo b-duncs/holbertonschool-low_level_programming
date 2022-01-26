62a6776d44a5345b083176c724fa4ddcbe5ce554
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 13:54:33 -0800

    updated 8-24_hours and fixed format issue

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min < 60; min++)
 		{
-			_putchar((hour % 10) + '0');
 			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((min % 10) + '0');
 			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 
