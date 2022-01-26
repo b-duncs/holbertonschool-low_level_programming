0ba2010006ca9aa3b4a31a21e70b4139c1f6201c
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 14:39:00 -0800

    added 9-times_table.c and 9-main.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,27 @@
-#include "main.h"
-
 /**
- * add -
- *
- * Return: 
+ * times_table - program startup
+(*
+ * Description: prints the nine times table starting with zero)?
  */
-int add(int, int)
-{
 
+#include "main.h"
+void times_table(void)
+{
+	int width
+	int length
+
+	for (width = 0; width <= 10; width++)
+	{
+		for (length = 0; length <= 10; length++)
+		{
+			if (length <= (length - 1))
+			{
+				printf("%i, ", (length * width));
+			}
+			else
+			{
+				printf("%i, ", (length * width));
+			}
+		}
+	}
 }
