725563875be9a5a27a3e9199436f25e800a0b166
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 13:48:37 -0800

    added 8-24_hours.c and 8-main.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,25 @@
-#include "main.h"
-
 /**
- * times_table -
- *
- * Return: 
+ * jack_bauer - program startup
+(*
+ * Description: prints every minute of the day
+ * starting from 00:00 to 23:59)?
  */
-void times_table(void)
-{
 
+#include "main.h"
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+	
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; minute++)
+		{
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+
+	}
 }
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     jack_bauer();
