17eedda9576a009876443779a01ddecf9664be18
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 10:26:09 -0800

    updated 2-print_alphabet_x10.c and fixed syntax error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int stepper;
 	char alpha;
 
-	for (stepper = 0, stepper <= 9; stepper++)
+	for (stepper = 0; stepper <= 9; stepper++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
