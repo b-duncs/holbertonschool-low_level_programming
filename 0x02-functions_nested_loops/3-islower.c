c07194ac3ce745088e4e27b928bc7286bdd656ec
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 10:22:40 -0800

    added 3-islower.c and 3-main.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * _isalpha -
- *
- * Return: 
+ * _islower - program startup
+ * @c: int being evaluated
+(*
+ * Description: checks for a lowercase character
+ * and returns `1` if `c` is lowecase
+ * and returns `0` otherwise)?
+ * Return: return (0) exits program successfully, return (1) throws an error
  */
-int _isalpha(int c)
-{
 
+#include "main.h"
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
