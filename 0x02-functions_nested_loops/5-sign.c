57101737606ec5dcc12ff8e397bbf678d6968706
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 11:19:05 -0800

    added 5-sign.c and 5-main.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,30 @@
-#include "main.h"
-
 /**
- * _abs -
- *
- * Return: 
+ * print_sign - program startup
+ * @n: int being evaluated
+(*
+ * Description: prints the sign of a number
+ * and returns `1` and prints `+` if `n` is greater than zero
+ * and returns `0` and prints `0` if `n` is zero
+ * and returns `-1` and prints `-` if `n` is less than zero?
+ * Return: return (0) exits program successfully, return (1) throws an error
  */
-int _abs(int)
-{
 
+#include "main.h"
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (c < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
 }
