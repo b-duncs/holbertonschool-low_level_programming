e89d5664c7fed560f5ceddb35cd2bb27a2132468
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 10:05:12 -0800

    added 2-print_alphabet_x10.c and 2-main.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,21 @@
-#include "main.h"
-
 /**
- * _islower -
- *
- * Return: 
+ * print_alphabet_x10 - program startup
+(*
+ * Description: prints the alphabet in lowercase ten times)?
  */
-int _islower(int c)
-{
 
+#include "main.h"
+void print_alphabet_x10(void)
+{
+	int stepper;
+	char alpha;
+
+	for (stepper = 0, stepper <= 9; stepper++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	}
+	_putchar('\n');
 }
