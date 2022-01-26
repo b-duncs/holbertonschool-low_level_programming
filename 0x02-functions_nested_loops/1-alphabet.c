34c6445a73ec6231674172769e89478d2daba80a
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 09:56:05 -0800

    added 1-alphabet.c and 1-main.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
-#include "main.h"
-
 /**
- * print_alphabet_x10 -
- *
- * Return: 
+ * print_alphabet - program startup
+(*
+ * Description: prints the alphabet in lowercase)?
  */
-void print_alphabet_x10(void)
-{
 
+#include "main.h"
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
 }
