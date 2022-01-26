6641bf1717e3e898fa1d7f6b7affe26a2ed6d98f
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 12:48:10 -0800

    updated 7-print_last_digit.c to account for negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,17 @@
 #include "main.h"
 int print_last_digit(int n)
 {
-	n = (n % 10);
-	_putchar(n + '0');
-	return (n);
+	if (n >= 0)
+	{
+		n = (n % 10);
+		_putchar(n + '0');
+		return (n);
+	}
+	else if (n < 0)
+	{
+		n = (-n % 10);
+		_putchar(n + '0');
+		return (n);
+	}
+	return (0);
 }
