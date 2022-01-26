8b2aea77fab96347bba2aa72aa87f0dad675b7f2
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 11:25:33 -0800

    updated 5-signs.c and changed putchar to _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	return (0);
