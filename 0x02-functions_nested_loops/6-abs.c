d09a52010a48f74928f7d7fdd2212d72a5c31be5
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 12:20:09 -0800

    added 6-abs.c and 6-main.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * print_last_digit -
- *
- * Return: 
+ * _abs - program startup
+ *  @n: int being evaluated
+(*
+ * Description: computes the absolute value of an integer?
+ * Return: return (0) is the required function signature
  */
-int print_last_digit(int)
-{
 
+#include "main.h"
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else if (n < 0)
+	{
+		n = (n * -1);
+		_putchar(n);
+	}
+	return (0);
 }
