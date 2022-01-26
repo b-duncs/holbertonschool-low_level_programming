d25bdb28a08521ba45d759f7806d7fc8540830f0
b-duncs <baylee.duncan@protonmail.com>
2022-01-26 13:25:49 -0800

    updated 7-print_last_digit.c to account for negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,13 @@ int print_last_digit(int n)
 	n = (n % 10);
 	if (n >= 0)
 	{
-		_putchar('0' + n);
+		_putchar(n + '0');
 		return (n);
 	}
 	else if (n < 0)
 	{
-		_putchar('0' - n);
-		return (n);
+		_putchar(n + '0');
+		return (-n);
 	}
 	return (0);
 }
