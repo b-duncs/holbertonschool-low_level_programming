f084bdc1b40d59e1335261cb49bf814e9eb7cbaf
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 10:46:49 -0800

    added 0-main.c and 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * _isupper -
- *
- * Return: 
+ * _isupper - program startup
+ * @c: int being evaluated
+(*
+ * Description: checks for an uppercase character
+ * and returns `1` if `c` is uppercase
+ * and returns `0` otherwise)?
+ * Return: return (0) exits program successfully, return (1) throws an error
  */
+
+#include "main.h"
 int _isupper(int c)
 {
-
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
