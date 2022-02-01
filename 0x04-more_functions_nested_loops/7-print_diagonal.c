9fa779b777294fc8cd7ab84e40135d61e196a9cd
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 15:59:55 -0800

    added 7-main.c and 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * print_diagonal -
- *
- * Return: 
+ * print_diagonal - program startup
+ * @n: int being evaluate
+(*
+ * Description: draws a diagonal line in the terminal)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_diagonal(int n)
 {
+	int diagonal;
 
+	if (n > 0)
+	{
+		for (diagonal = 0; diagonal < n; diagonal++)
+		{
+		_putchar(47);
+		}
+	}
+	_putchar('\n');
 }
