e5e42d6e18b697b5a941fbe8518c2ced0dbc5c1d
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 11:09:48 -0800

    added 2-main.c and 2-mul.c

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     printf("%d\n", mul(98, 1024));
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,17 @@
-#include "main.h"
-
 /**
- * mul -
- *
- * Return: 
+ * mul - program startup
+ * @a: first int being evaluated
+ * @b: second int being evaluated
+(*
+ * Description: multiplies two integers and returns the results)?
+ * Return: product
  */
+
+#include "main.h"
 int mul(int a, int b)
 {
+	int product;
 
+	product = x * y;
+	return (product);
 }
