b1a5a42751f7fe040bdfab1f6d27c50445174811
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 14:44:21 -0800

    updated 5-more_numbers and fixed syntaxt error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 /**
  * more_numbers - program startup
 (*
- * Description: prints ten times the numbers `0` to `14` 
+ * Description: prints ten times the numbers `0` to `14`
  * followed by a new line)?
  * Return: return (0) is the required function signature
  */
@@ -11,7 +11,7 @@ void more_numbers(void)
 {
 	int stepper;
 	int number;
- 
+
 	for (stepper = '0'; stepper <= 9; stepper++)
 	{
 		for (number = '0'; number <= '14'; number++)
@@ -26,5 +26,5 @@ void more_numbers(void)
 			}
 		}
 		_putchar('\n');
-	}	
+	}
 }
