f9e672219034b88e29f4a9b8c5791be5f2e9886a
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 14:36:20 -0800

    updated 5-more_numbers.c and fixed _putchar error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,14 @@ void more_numbers(void)
 	{
 		for (number = '0'; number <= '14'; number++)
 		{
-			_putchar(number % 10 + '0');
+			if (number <=9)
+			{
+				_putchar((number % 10) + '0');
+			}
+			else
+			{
+				_putchar((number / 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}	
