636755fe5c3392d4b77072bf84a65d616d2760a9
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 13:32:01 -0800

    added 3-main.c and 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * print_numbers -
- *
- * Return: 
+ * print_numbers - program startup
+(*
+ * Description: prints the numbers `0` to `9` followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_numbers(void)
 {
+	int number;
 
+	for (number == '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+		_putchar('/n');
+	}
+	else
+	{
+		return (0);
+	}
 }
