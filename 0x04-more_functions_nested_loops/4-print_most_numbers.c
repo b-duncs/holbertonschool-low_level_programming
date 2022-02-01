0fe1a6c2f96611fc219451b0cb9fde16cfe31fe7
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 13:49:51 -0800

    added 4-print_most_numbers.c and 4-main.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * print_most_numbers -
- *
- * Return: 
+ * print_most_numbers - program startup
+(*
+ * Description: prints the numbers `0` to `9` followed by a new line
+ * and does not print `2` and `4`)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_most_numbers(void)
 {
+	int number;
 
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number != 4 || number != 2)
+		{
+			_putchar(number);
+		}
+	}
+	_putchar('\n');
 }
