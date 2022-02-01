3e3edcc53890bb0ef8fae4db0bb6a9a717434dca
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 14:21:53 -0800

    added 5-main.c and 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-main.c
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     more_numbers();
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * more_numbers -
- *
- * Return: 
+ * more_numbers - program startup
+(*
+ * Description: prints ten times the numbers `0` to `14` followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void more_numbers(void)
 {
-
+	int stepper;
+	int number;
+ 
+	for (stepper = '0'; stepper <= 9; stepper++)
+	{
+		for (number = '0'; number <= '14'; number++)
+		{
+			_putchar(number);
+		}
+		_putchar('\n');
+	}	
 }
