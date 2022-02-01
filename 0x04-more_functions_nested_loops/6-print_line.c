da4cd7fa6dcef1908d022cadb3df5b9db67d8296
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 15:38:09 -0800

    added 6-main.c and 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * print_line -
- *
- * Return: 
+ * print_line - program startup
+ * @n: int being evaluate
+(*
+ * Description: draws a straight line in the terminal)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_line(int n)
 {
+	int line;
 
+	if (n > 0)
+	{
+		for (line = 0; line <= n; line++)
+		{
+		_putchar(95);
+		}
+	}
+	_putchar('\n');
 }
