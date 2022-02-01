e123ba821e56120d7d0c793a5a895c6d26575a11
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 13:36:39 -0800

    updated 3-print_numbers.c and fixed syntax errors

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,13 +10,9 @@ void print_numbers(void)
 {
 	int number;
 
-	for (number == '0'; number <= '9'; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
 		_putchar(number);
-		_putchar('/n');
 	}
-	else
-	{
-		return (0);
-	}
+	_putchar('\n');
 }
