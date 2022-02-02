a295f30481fe61b93a0f1423ce8e68abec84abb5
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 17:00:43 -0800

    added 8-main.c and 8-print_squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,11 +1,25 @@
-#include "main.h"
-
 /**
- * print_square -
- *
- * Return: 
+ * print_square - program startup
+ * @size: int being evaluate
+(*
+ * Description: prints a square followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_square(int size)
 {
+	int square;
+	int spacer;
 
+	{
+		for (square = 0; square < size; square++)
+		{
+			for (spacer = 0; spacer < size; spacer++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
