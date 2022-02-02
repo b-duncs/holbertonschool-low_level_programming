e644f8c7a3efb95c0f667fa900c135b29f47ba1b
b-duncs <baylee.duncan@protonmail.com>
2022-02-02 13:42:56 -0800

    added 6-main.c and 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,25 @@
-#include "main.h"
-
 /**
- * puts2 -
- *
- * Return: 
+ * puts2 - program startup
+ * @str: pointer being evaluated
+(*
+ * Description: prints every other character of a string
+ * starting with the first character followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void puts2(char *str)
 {
+	int length;
+	int string;
 
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		continue;
+	}
+	for (string = 0; string < length; string += 2)
+	{
+		_putchar(str[string]);
+	}
+	_putchar('\n');
 }
