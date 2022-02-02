4f3b998e2907b623daa1c6e2fb6a46dee9c66c61
b-duncs <baylee.duncan@protonmail.com>
2022-02-02 11:41:25 -0800

    added 3-main.c and 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,18 @@
-#include "main.h"
-
 /**
- * _puts -
- *
- * Return: 
+ * _puts - program startup
+ * @str: pointer being evaluated
+(*
+ * Description: prints a string followed by a new line
+ * to the standard output)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void _puts(char *str)
 {
-
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
