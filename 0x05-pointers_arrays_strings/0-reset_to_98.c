3d0ac8653a9934067be3f25bb8cbe3c778432a88
b-duncs <baylee.duncan@protonmail.com>
2022-02-02 10:25:58 -0800

    added 0-main.c and 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,14 @@
-#include "main.h"
-
 /**
- * reset_to_98 -
- *
- * Return: 
+ * reset_to_98 - program startup
+ * @*n: pointer being evaluated
+(*
+ * Description: takes a pointer to an `int` as parameter
+ * and updates the value it points to to `98`)?
+ * Return: return (0) is the required function signature
  */
-void reset_to_98(int *n)
-{
 
+#include "main.h"
+int reset_to_98(int *n)
+{
+	*n = 98;
 }
