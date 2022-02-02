4b62c672476042187e185d5c3c1dbc3e33152e4e
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 19:18:46 -0800

    updated 9-fizz_buzz.c and fixed syntax error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main(void)
 	{
 		if (((x % 3) == 0) && ((x % 5) == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if ((x % 3) == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if ((x % 5) == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", x);
+			printf(" %d", x);
 		}
 	}
 	putchar('\n');
