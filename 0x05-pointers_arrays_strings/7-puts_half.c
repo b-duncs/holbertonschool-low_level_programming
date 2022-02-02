b88008b2919a8c11072eedc52ec06260a2365919
b-duncs <baylee.duncan@protonmail.com>
2022-02-02 14:21:54 -0800

    added 7-main.c and 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,33 @@
-#include "main.h"
-
 /**
- * puts_half -
- *
- * Return: 
+ * puts_half - program startup
+ * @str: pointer being evaluated
+(*
+ * Description: prints half of a string followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void puts_half(char *str)
 {
+	int length;
+	int string;
+	int half;
 
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		continue;
+	}
+	if ((length % 2) == 0)
+	{
+		half = (length / 2);
+	}
+	else
+	{
+		half = ((length + 1) / 2);
+	}
+	for (string = half; string < length; string++)
+	{
+		_putchar(str[string]);
+	}
+	_putchar('\n');
 }
