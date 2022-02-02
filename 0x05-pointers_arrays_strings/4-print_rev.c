e7b0c7f241dd90901f88877d99aa51588eddc6bc
b-duncs <baylee.duncan@protonmail.com>
2022-02-02 12:46:12 -0800

    added 4-main.c and 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,24 @@
-#include "main.h"
-
 /**
- * print_rev -
- *
- * Return: 
+ * print_rev - program startup
+ * @s: pointer being evaluated
+(*
+ * Description: prints a string in reverse followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_rev(char *s)
 {
+	int length;
+	int string;
 
+	for (length = 0; s[length] != '\0'; length++)
+	{
+		continue;
+	}
+	for (string = length - 1; string >= 0; string--)
+	{
+		_putchar(s[string]);
+	}
+	_putchar('\n');
 }
