7a738f79ec76796ebabfc537ba6c01bde015a421
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 17:32:08 -0800

    updated 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if (((x % 3) == 0) && ((x % 5) == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if (x % 3 == 0)
+		else if ((x % 3) == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (x % 5 == 0)
+		else if ((x % 5) == 0)
 		{
 			printf("Buzz ");
 		}
