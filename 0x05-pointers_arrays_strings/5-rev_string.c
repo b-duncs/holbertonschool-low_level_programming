5e695462906e5f6d04e4240d1132bdc818da3109
b-duncs <baylee.duncan@protonmail.com>
2022-02-02 13:14:05 -0800

    added 5-main.c and 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,26 @@
-#include "main.h"
-
 /**
- * rev_string -
- *
- * Return: 
+ * rev_string - program startup
+ * @s: pointer being evaluated
+(*
+ * Description: reverses a string)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void rev_string(char *s)
 {
+	int length;
+	int string;
+	char reverse;
 
+	for (length = 0; s[length] != '\0'; length++)
+	{
+		continue;
+	}
+	for (string = length - 1; string >= length / 2; string--)
+	{
+		reverse = s[string];
+		s[string] = s[length - string - 1];
+		s[length - string - 1] = reverse;
+	}
 }
