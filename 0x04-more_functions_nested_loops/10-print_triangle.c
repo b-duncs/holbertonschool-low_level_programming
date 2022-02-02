48d4d694e58b3a0891010ccba743526df274ffd5
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 20:22:57 -0800

    added 10-print_triangle.c and 10-main.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+/**
+ * print_triangle - program startup
+ * @size: int being evaluate
+(*
+ * Description: prints a triangle followed by a new line)?
+ * Return: return (0) is the required function signature
+ */
+
+#include "main.h"
+void print_triangle(int size)
+{
+	int triangle;
+	int spacer;
+
+	if (size > 0)
+	{
+		for (triangle = 1; triangle <= size; triangle++)
+		{		
+			for (spacer = 1; spacer <= size; spacer++)
+			{
+				if (spacer <= size - triangle)
+				{
+					_putchar(32);
+				}
+				if (triangle + spacer >= size +1)
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
