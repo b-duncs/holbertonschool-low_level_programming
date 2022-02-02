3385814ac9452075c5d193115034a3b453d4b3b7
b-duncs <baylee.duncan@protonmail.com>
2022-02-01 17:29:46 -0800

    added 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,34 @@
-#include "main.h"
-
 /**
- * print_triangle -
- *
- * Return: 
+ * main - program startup
+(*
+ * Description: standard fizzbuzz program)?
+ * Return: return (0) is the required function signature
  */
-void print_triangle(int size)
-{
 
+#include "main.h"
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
