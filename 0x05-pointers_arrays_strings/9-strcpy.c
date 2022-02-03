dc562d418115ba37a79d6693cd7ac264fa3aa7ba
b-duncs <baylee.duncan@protonmail.com>
2022-02-02 18:49:38 -0800

    updated 9-strcpy.c and added check for null term

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -17,9 +17,6 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[string] = src[string];
 	}
-	for (string = 0; src[string] == '\0'; string++)
-	{
-		dest[string] = '\0';
-	}
+	dest[string++] = '\0';
 	return (dest);
 }
