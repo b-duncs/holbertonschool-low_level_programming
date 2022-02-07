07cc0f9449ce7c791052963195e12f8fbebfd8c1
b-duncs <baylee.duncan@protonmail.com>
2022-02-07 11:06:23 -0800

    added 2-main.c and 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     char s1[98];
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,29 @@
-#include "holberton.h"
-
 /**
- * _strncpy -
- *
- * Return: 
+ * *_strncpy - program startup
+ * @dest: first pointer being evaluated
+ * @src: second pointer being evaluated
+ * @n: first pointer being evaluated
+(*
+ * Description: concatenates two strings)?
+ * Return: return (0) is the required function signature
  */
+
+#include "holberton.h"
 char *_strncpy(char *dest, char *src, int n)
 {
+	int length;
 
+	for (length = 0; length < n; length++)
+	{
+		dest[length] = src[length];
+		if (src[length] == '\0')
+		{
+			while (length < n)
+			{
+				dest[length] = '\0';
+				length++;
+			}
+		}
+	}
+	return (dest);
 }
