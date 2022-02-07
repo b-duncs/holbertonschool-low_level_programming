662248d725d13ce54a3a644d427e5cb9bd14a124
b-duncs <baylee.duncan@protonmail.com>
2022-02-07 11:51:58 -0800

    added 3-main.c and 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,24 @@
-#include "holberton.h"
-
 /**
- * _strcmp -
- *
- * Return: 
+ * _strcmp - program startup
+ * @s1: first pointer being evaluated
+ * @s2: second pointer being evaluated
+(*
+ * Description: compares two strings)?
+ * Return: return (0) is the required function signature
  */
+
+#include "holberton.h"
 int _strcmp(char *s1, char *s2)
 {
+	int string;
 
+	for (string = 0; s1[string] != '\0' && s2[string] != '\0'; string++)
+	{
+		if (*s1 - *s2 == 0)
+		{
+			s1++;
+			s2++;
+		}
+	}
+	return (*s1 - *s2);
 }
