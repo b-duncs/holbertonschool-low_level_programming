085277ea945769c42c20df9d73ca5566e520eff0
b-duncs <baylee.duncan@protonmail.com>
2022-02-07 12:22:43 -0800

    added 4-main.c and 4-rev_arrays.c

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -8,6 +8,7 @@
  *
  * Return: nothing.
  */
+
 void print_array(int *a, int n)
 {
     int i;
@@ -30,6 +31,7 @@ void print_array(int *a, int n)
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,22 @@
-#include "holberton.h"
-
 /**
- * reverse_array -
- *
- * Return: 
+ * _strcmp - program startup
+ * @a: pointer being evaluated
+ * @n: integer being evaluated
+(*
+ * Description: reverses the content of an array of integers)?
+ * Return: return (0) is the required function signature
  */
+
+#include "holberton.h"
 void reverse_array(int *a, int n)
 {
+	int string;
+	int reverse;
 
+	for (string = n - 1; string >= n / 2; string--)
+	{
+		reverse = a[string];
+		a[string] = a[n - 1 - string];
+		a[n - 1 - string] = reverse;
+	}
 }
