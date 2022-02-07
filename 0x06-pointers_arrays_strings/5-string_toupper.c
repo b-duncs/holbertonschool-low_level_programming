06ec7ddba9a9bb7d20a19ef703ae09c225ec828d
b-duncs <baylee.duncan@protonmail.com>
2022-02-07 12:55:09 -0800

    added 5-main.c and 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,23 @@
-#include "holberton.h"
-
 /**
- * string_toupper -
- *
- * Return: 
+ * *string_toupper - program startup
+ * @a: pointer being evaluated
+ * (*
+ * Description: changes all lowercase letters of a string to uppercase)?
+ * Return: return (0) is the required function signature
  */
-char *string_toupper(char *)
-{
 
+#include "holberton.h"
+char *string_toupper(char *a)
+{
+	int string;
+
+	for (string = 0; a[string] != '\0';)
+	{
+		if (a[string] >= 'a' && a[string] <= 'z')
+		{
+			a[string] = a[string] - 32;
+		}
+		string++;
+	}
+	return (a);
 }
