3dc7ef30bdbd3ace1954396f22feae2db69e9753
b-duncs <baylee.duncan@protonmail.com>
2022-02-07 09:31:50 -0800

    added 0-main.c and 0-strcat.c

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     char s1[98] = "Hello ";
@@ -19,12 +20,4 @@ int main(void)
     printf("%s", s2);
     printf("%s", ptr);
     return (0);
-}
-julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-strcat.c -o 0-strcat
-julien@ubuntu:~/0x06$ ./0-strcat 
-Hello 
-World!
-Hello World!
-World!
-Hello World!
-julien@ubuntu:~/0x06$ 
+} 
--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,26 @@
-#include "holberton.h"
-
 /**
- * _strcat -
- *
- * Return: 
+ * *_strcat - program startup
+ * @dest: first pointer being evaluated
+ * @src: second pointer being evaluated
+(*
+ * Description: concatenates two strings)?
+ * Return: return (0) is the required function signature
  */
+
+#include "holberton.h"
 char *_strcat(char *dest, char *src)
 {
+	int length;
+	int string;
 
+	for (length = 0; dest[length] != '\0'; length++)
+	{
+		continue;
+	}
+	for (string = 0; src[string] != '\0'; string++)
+	{
+		dest[length++] = src[string];
+	}
+	dest[length] = '\0';
+	return (dest);
 }
