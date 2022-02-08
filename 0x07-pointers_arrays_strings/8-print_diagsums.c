512fde3c8a801c88f2cbb1c1a083464cf26fcc20
b-duncs <baylee.duncan@protonmail.com>
2022-02-08 15:17:03 -0800

    added 8-main.c and 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,24 @@
-#include "main.h"
-
 /**
- * print_diagsums -
- *
- * Return: 
+ * print_diagsums - program startup
+ * @a: pointer being evaluated
+ * @size: int being evaluated
+(*
+ * Description: prints the sum of the two diagonals
+ * of a square matrix of integers)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_diagsums(int *a, int size)
 {
+	int sum;
+	unsigned int x = 0;
+	unsigned int y = 0;
 
+	for (sum = 0; sum < size; sum++)
+	{
+		x = x + a[(size * sum) + sum];
+		y = y + a[(size * (sum + 1)) - (sum + 1)];
+	}
+	printf("%d, %d\n", x, y);
 }
