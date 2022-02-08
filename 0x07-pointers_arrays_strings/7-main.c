206d44c79ded222514746b47d84dbd078754fcf2
b-duncs <baylee.duncan@protonmail.com>
2022-02-08 15:05:34 -0800

    added 7-main.c and 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-main.c
+++ 0x07-pointers_arrays_strings/7-main.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     char board[8][8] = {
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,23 @@
-#include "main.h"
-
 /**
- * print_chessboard -
- *
- * Return: 
+ * print_chessboard - program startup
+ * @a: pointer being evaluated
+(*
+ * Description: prints the chessboard)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void print_chessboard(char (*a)[8])
 {
+	int length;
+	int height;
 
+	for (length = 0; a[length][7]; length++)
+	{
+		for (height = 0; height < 8; height++)
+		{
+			_putchar(a[length][height]);
+		}
+		_putchar('\n');
+	}
 }
