3f218c9765162c644479294a854dedbd3d8ccbaf
b-duncs <baylee.duncan@protonmail.com>
2022-02-08 10:57:38 -0800

    added 1-main.c and 1.memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,21 @@
-#include "main.h"
-
 /**
- * _memcpy -
- *
- * Return: 
+ * *_memcpy - program startup
+ * @dest: first pointer being evaluated
+ * @src: second pointer being evaluated
+ * @n: int being evaluated
+(*
+ * Description: copies memory area)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int byte;
 
+	for (byte = 0; byte < n; byte++)
+	{
+		src[byte] = dest[byte];
+	}
+	return (dest);
 }
