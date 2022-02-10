46a196cc74b5dfdb5f6af146f2adef0718129250
b-duncs <baylee.duncan@protonmail.com>
2022-02-09 17:12:14 -0800

    added 1-main.c and 1-print_rev_recursion.c

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     _print_rev_recursion("\nColton Walker");
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,17 @@
-#include "main.h"
-
 /**
- * _print_rev_recursion -
- *
- * Return: 
+ * _print_rev_recursion - program startup
+ * @s: pointer being evaluated
+(*
+ * Description: prints a string in reverse)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void _print_rev_recursion(char *s)
 {
-
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
