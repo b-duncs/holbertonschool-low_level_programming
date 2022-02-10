083d50190d2a15d2e144f51a1462c1ca8c470fcf
b-duncs <baylee.duncan@protonmail.com>
2022-02-09 17:33:13 -0800

    added 2-main.c and 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,17 @@
-#include "main.h"
-
 /**
- * _strlen_recursion -
- *
- * Return: 
+ * _strlen_recursion - program startup
+ * @s: pointer being evaluated
+(*
+ * Description: returns the length of a string)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 int _strlen_recursion(char *s)
 {
-
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1); 
+	}
+	return (0);
 }
