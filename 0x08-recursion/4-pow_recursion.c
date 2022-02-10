120b58e80a39f6af8bb0ee0130ff9ab3c3b99290
b-duncs <baylee.duncan@protonmail.com>
2022-02-09 17:58:53 -0800

    added 4-main.c and 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,25 @@
-#include "main.h"
-
 /**
- * _pow_recursion -
- *
- * Return: 
+ * _pow_recursion - program startup
+ * @x: first int being evaluated
+ * @y: second int being evaluated
+(*
+ * Description: returns the value of `x` raised to the power of `y`)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
