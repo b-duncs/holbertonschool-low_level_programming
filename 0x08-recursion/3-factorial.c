6b579d9fcf972e05caf6edcecc2d3d31da2fd641
b-duncs <baylee.duncan@protonmail.com>
2022-02-09 17:45:25 -0800

    added 3-main.c and 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,25 @@
-#include "main.h"
-
 /**
- * factorial -
- *
- * Return: 
+ * factorial - program startup
+ * @n: int being evaluated
+(*
+ * Description: returns the factorial of a given number)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 int factorial(int n)
 {
-
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else if (n >= 1)
+	{
+		return (n * factorial(n - 1));
+	}
+	return (0);
 }
