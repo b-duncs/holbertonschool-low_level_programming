c0d08371f1e381a3f06e1f00dc8a063cfb9794e2
b-duncs <baylee.duncan@protonmail.com>
2022-02-15 18:18:40 -0800

    added 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+/**
+ * main - program startup
+ * @argc: argument count
+ * @argv: argument vector
+(*
+ * Description: prints its own program name followed by a new line)?
+ * Return: return (0) is the required function signature
+ */
+
+#include "main.h"
+int main(int argc, char *argv[])
+{
+	int argument;
+
+	for (argument = 0; argument < argc; argument++)
+	{
+		printf("%s\n", argv[argument]);
+	}
+	return (0);
+}
