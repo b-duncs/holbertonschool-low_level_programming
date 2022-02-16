ed2c3de0722e8041100f0037be1e6e1d4042b33a
b-duncs <baylee.duncan@protonmail.com>
2022-02-16 13:11:41 -0800

    added 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,50 @@
-#include "main.h"
-
 /**
- * alloc_grid -
- *
- * Return: 
+ * **alloc_grid - program startup
+ * @width: first int being evaluated
+ * @height: second int  being evaluated
+(*
+ * Description: returns a pointer to a two dimensional array of integers)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 int **alloc_grid(int width, int height)
 {
+	int **array;
+	int x;
+	int y;
 
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(int *) * height);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < height; x++)
+	{
+		array[x] = malloc(sizeof(int) * width);
+		if (array[x] == NULL)
+		{
+			free(array);
+			for (y = 0; y <= x; y++)
+			{
+				free(array[y]);
+			}
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+		{
+			array[x][y] = 0;
+		}
+	}
+	return (array);
 }
+
