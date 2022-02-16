3f6293bbc6e868245ac66b03f9f0af2c9d264230
b-duncs <baylee.duncan@protonmail.com>
2022-02-16 11:55:13 -0800

    added 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,28 @@
-#include "main.h"
-
 /**
- * create_array -
- *
- * Return: 
+ * *create_array - program startup
+ * @size: int being evaluated
+ * @c: char being evaluated
+(*
+ * Description: creates an array of chars
+ * and initializes it with a specific char)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int rand;
 
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	for (rand = 0; rand < size; rand++)
+	{
+		array[rand] = c;
+	}
+	return (array);
 }
