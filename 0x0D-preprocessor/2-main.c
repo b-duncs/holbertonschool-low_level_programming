521685767050e02ac987faac5ca21122e54832ef
b-duncs <baylee.duncan@protonmail.com>
2022-02-17 11:48:46 -0800

    added 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - program startup
+(*
+ * Description: prints the name of the file it is compiled from)?
+ * Return: return (0) is the required function signature
+ */
+
+#include "main.h"
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
