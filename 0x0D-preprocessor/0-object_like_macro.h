04ad5167d03221dac5891d814a82fa2b5ab2da39
b-duncs <baylee.duncan@protonmail.com>
2022-02-17 11:45:00 -0800

    updated 0-object_like_macro.h and fixed syntax error

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,9 +1,9 @@
-#ifndef OBJECT_LIKE_MACRO
-#define OBJECT_LIKE_MACRO
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SIZE 1024
 
-#endif /* OBJECT_LIKE_MACRO */
+#endif /* OBJECT_LIKE_MACRO_H */
