b9f0db776bb18fb827c6e9f8ac793826388c3810
b-duncs <baylee.duncan@protonmail.com>
2022-02-16 18:39:10 -0800

    added 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,11 +1,21 @@
-#include "main.h"
-
 /**
- * free_grid -
- *
- * Return: 
+ * free_grid - program startup
+ * @grid: pointer being evaluated
+ * @height: int being evaluated
+(*
+ * Description: frees a two dimensional grid previously created
+ * by the `alloc_grid` function)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void free_grid(int **grid, int height)
 {
-
+	int x;
+	
+	for(x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+	free(grid);
 }
