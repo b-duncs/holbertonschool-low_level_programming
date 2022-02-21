ad05b2bd2c6c45415130dcac75ab3b1378cc813f
b-duncs <baylee.duncan@protonmail.com>
2022-02-21 13:43:08 -0800

    added 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,19 @@
-#include "main.h"
-
 /**
- * malloc_checked -
- *
- * Return: 
+ * *malloc_checked - program startup
+ * @b: int being evaluated
+(*
+ * Description: allocates memory using malloc)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 void *malloc_checked(unsigned int b)
 {
-
+	void *memory = malloc(b);
+	
+	if (memory == NULL)
+	{
+		exit(98);
+	}
+	return (memory);
 }
