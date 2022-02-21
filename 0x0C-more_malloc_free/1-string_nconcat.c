db459daffb18d03a897c478e292cb3d9db37270c
b-duncs <baylee.duncan@protonmail.com>
2022-02-21 14:32:48 -0800

    updated 1-string_nconcat.c and fixed syntax errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,6 +28,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		length++;
 	}
 	concat = malloc(sizeof(char) * (length + 1));
+	if (concat == NULL)
+	{
+		return NULL;
+	}
 	length = 0;
 	for (string = 0; s1[string]; string++)
 	{
