d5ff176d1d88ef4bdb8060477bb5e548f826e6d8
b-duncs <baylee.duncan@protonmail.com>
2022-02-23 10:15:52 -0800

    added 0-main.c and 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,18 @@
-#include "function_pointers.h"
-
 /**
- * print_name -
- *
- * Return: 
+ * print_name - program startup
+ * @name: char being evaluated
+ * @f: function being evaluated
+(*
+ * Description: prints a name)?
+ * Return: return (0) is the required function signature
  */
+
+#include "function_pointers.h"
 void print_name(char *name, void (*f)(char *))
 {
-
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
 }
