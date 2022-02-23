2ba3b500a4d77f11211d9a4ea55fa9590658bcd9
b-duncs <baylee.duncan@protonmail.com>
2022-02-23 12:26:18 -0800

    updated 0-print_name.c and changed if structure

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,11 +10,7 @@
 #include "function_pointers.h"
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	else
+	if (name != NULL && f = NULL)
 	{
 		f(name);
 	}
