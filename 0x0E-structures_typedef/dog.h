fe8e45d5dde8f78c5207883f92664f3d67a46ab0
b-duncs <baylee.duncan@protonmail.com>
2022-02-22 20:52:10 -0800

    added dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
-#include " files might be different from the one shown in the examples"
+#ifndef DOG_H
+#define DOG_H
 
-/**
- * free_dog -
- *
- * Return: 
- */
-void free_dog(dog_t *d)
+#include <stdio.h>
+#include <stdlib.h>
+
+struct dog
 {
+	char = *name;
+	float = age;
+	char = *owner;
+};
 
-}
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
