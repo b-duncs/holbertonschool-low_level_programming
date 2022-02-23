18250b0095db5119aca433a89dae69dabce1d0b8
b-duncs <baylee.duncan@protonmail.com>
2022-02-22 18:06:40 -0800

    added 3-main.c and 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,11 +1,32 @@
-#include "main.h"
-
 /**
- * array_range -
- *
- * Return: 
+ * *array_range - program startup
+ * @min: first int being evaluated
+ * @max: second int being evaluated
+(*
+ * Description: creates an array of integers)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 int *array_range(int min, int max)
 {
+	int *array;
+	int string;
+	int size;
 
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = max - min + 1;
+	array = malloc(sizeof(int) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (string = 0; string < size; string++)
+	{
+		array[string] = min++;
+	}
+	return (array);
 }
--- 0x0C-more_malloc_free/3-main.c
+++ 0x0C-more_malloc_free/3-main.c
@@ -10,6 +10,7 @@
  *
  * Return: Nothing.
  */
+
 void simple_print_buffer(int *buffer, unsigned int size)
 {
     unsigned int i;
