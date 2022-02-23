0d1b0783d1144079867bfb1ed877ec8921b73512
b-duncs <baylee.duncan@protonmail.com>
2022-02-22 21:20:35 -0800

    added 1-main.c and 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,21 @@
-#include " files might be different from the one shown in the examples"
-
 /**
- * print_dog -
- *
- * Return: 
+ * init_dog - program startup
+ * @d: struct being evaluated
+ * @name: first char being evaluated
+ * @owner: second char being evaluated
+ * @age: float being evaluated
+(*
+ * Description: initializes a variable of type struct dog)?
+ * Return: return (0) is the required function signature
  */
-void print_dog(struct dog *d)
-{
 
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     struct dog my_dog;
