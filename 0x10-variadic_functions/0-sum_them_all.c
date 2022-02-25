49558e33529f82ec5e86bbdc3c6bce5dd85fc2a8
b-duncs <baylee.duncan@protonmail.com>
2022-02-25 15:10:30 -0800

    added 0-main.c and 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,24 @@
-#include "variadic_functions.h"
-
 /**
- * sum_them_all -
- *
- * Return: 
+ * sum_them_all - program startup
+ * @n: int being evaluated
+(*
+ * Description: returns the sum of all its parameters)?
+ * Return: return (0) is the required function signature
  */
+
+#include "variadic_functions.h"
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list params;
+	unsigned int string, sum;
 
+	va_start(params, n);
+
+	sum = 0;
+	for (string = 0; string < n; string++)
+	{
+		sum += va_arg(params, int);
+	}
+	va_end(params);
+	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
