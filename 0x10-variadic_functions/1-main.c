c85ce6f01ae2d4a1b71b01ef8f84fa647e17e2e5
b-duncs <baylee.duncan@protonmail.com>
2022-02-25 15:18:35 -0800

    added 1-main.c and 1-print_numbers.c

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     print_numbers(", ", 4, 0, 98, -1024, 402);
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,28 @@
-#include "variadic_functions.h"
-
 /**
- * print_numbers -
- *
- * Return: 
+ * print_numbers - program startup
+ * @separator: char being evaluated
+ * @n: int being evaluated
+(*
+ * Description: prints numbers followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "variadic_functions.h"
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list numbers;
+	unsigned int string;
 
+	va_start(numbers, n);
+
+	for (string = 0; string < n; string++)
+	{
+		printf("%d", va_arg(numbers, int));
+		if (string != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(numbers);
 }
