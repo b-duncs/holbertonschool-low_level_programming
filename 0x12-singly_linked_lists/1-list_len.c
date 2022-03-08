f4a7bc749fab9da67c561f55ce8519e9c1641a55
b-duncs <4217@holbertonschool.com>
2022-03-08 10:13:49 -0800

    added 1-main.c and 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,21 @@
-fskdjsd
+#include "lists.h"
+
+/**
+ * list_len - program startup
+ * @h: list being evaluated
+(*
+ * Description: returns the number of elements in a linked `list_t` list)?
+ * Return: return (0) is the required function signature
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements;
+
+	while (h != NULL)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
