7e787655fd32f68443cf3d9c4369df5f28c44fee
b-duncs <4217@holbertonschool.com>
2022-03-08 09:53:20 -0800

    added 0-main.c and 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - program startup
+ * @h: list being evaluated
+(*
+ * Description: prints all the elements of a 'list_t' list)?
+ * Return: return (0) is the required function signature
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		node_count++;
+		h = h->next;
+	}
+	return (node_count);
+}
