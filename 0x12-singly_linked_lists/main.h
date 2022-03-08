e17fb7db714a2dbcfbc685b92c2f6cde463b6eb5
b-duncs <baylee.duncan@protonmail.com>
2022-03-08 08:34:37 -0800

    updated main.h

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -5,25 +5,20 @@
 #include <stdlib.h>
 
 /**
- * struct list_t - program startup
- * @str: char being evaluated
- * @len: int being evaluated
- * @next: struct being evaluated
-(*
- * Description: defines new type struct list_t)?
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 
-struct list
+typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_t *next;
-}
-
-/**
- * struct_t - typedef for struct list
- */
-typedef struct list list_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
