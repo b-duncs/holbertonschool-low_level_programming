312d2947c943f6f187b21e008eeac6f638b598a6
b-duncs <4217@holbertonschool.com>
2022-03-09 21:38:54 -0800

    added 8-main.c and 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listin - program startup
+ * @head: list being evaluated
+(*
+ * Description: returns the sum of all the data (n)
+ * of a `listint_t` list)?
+ * Return: return (0) is the required function signature
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
