fd3560fa6090a234f8a7ee35cb713284ef782b1e
b-duncs <4217@holbertonschool.com>
2022-03-11 13:10:56 -0800

    updated 0-binary_to_uint.c and added loop for cd && ;s

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int x = 0;
+	int x;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-	while (b[x] == '0' || b[x] == '1')
+	for (x = 0; b[x] != '\0'; x++)
 	{
-		num <<= 1;
-		num += b[x] - '0';
-		x++;
+		if (b[x] == '0' || b[x] == '1')
+		{
+			num <<= 1;
+			num += b[x] - '0';
+		}
+		else
+		{
+			return (0);
+		}
 	}
 	return (num);
 }
