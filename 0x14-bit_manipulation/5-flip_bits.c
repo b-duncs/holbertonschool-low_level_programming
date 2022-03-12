4a3c0db9272dc36d6f830d82d421b0bdea1b26d8
b-duncs <4217@holbertonschool.com>
2022-03-11 21:59:31 -0800

    added 5-main.c and 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - program startup
+ * @n: first int being evaluated
+ * @m: second int being evaluated
+(*
+ * Description: returns the number of bits you would need
+ * to flip to get from one number to another)?
+ * Return: return (0) is the required function signature
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+	return (bits);
+}
