c2ca21aab0952fee64629eeac358bbe47b552e40
b-duncs <4217@holbertonschool.com>
2022-04-13 14:13:25 -0700

    updated 2-append_text_to_file.c and added if statement

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	int o = open(filename, O_WRONLY | O_APPEND);
 	int w = write(o, text_content, length);
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
 	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
