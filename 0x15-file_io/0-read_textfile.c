9fc95dcc183720e4a884ad8a6fbf8831a848c829
b-duncs <4217@holbertonschool.com>
2022-07-18 16:54:15 -0700

    added 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,23 +10,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	int nbyte;
-	char *buffer = malloc(sizeof(char) * letters);
+	ssize_t open, read, write;
+	char *buffer;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	file = open(filename, O_RDONLY);
-	if (file < 0)
-	{
-		return (0);
-	}
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(file);
 		return (0);
 	}
-	nbyte = read(file, buffer, letters);
+	open = open(filename, O_RDONLY);
+	read = read(open, buffer, letters);
+	write = write(STDOUT_FILENO, buffer, read);
+	if (open == -1 || read == -1 || write -1 || write != read)
+	{
+		free(buffer);
+		return (0);
+	}
+	free (buffer);
+	close(open);
+
+	return (write);
 }
