a608d6f3b7eebeab07cb1e4d4446a0faadc10ea7
b-duncs <4217@holbertonschool.com>
2022-07-18 17:02:49 -0700

    added files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys.stat.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
