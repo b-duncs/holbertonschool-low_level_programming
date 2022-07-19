3342e93abf28ab07b2201839a83a5cb0d920f9d1
b-duncs <4217@holbertonschool.com>
2022-07-18 18:11:47 -0700

    added 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - program startup
+ * @filename: text file
+ * @letters: number of letters to read and print
+(* Description: reads a text file and prints it to the POSIX standard output)?
+ * Return: the actual number of letters it could read and print
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write (o, text_content, len);
+	if (o == -1 || w == -1)
+	{
+		return (-1);
+	}
+	close (o);
+
+	return (1);
+}
