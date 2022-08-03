693b8f7013f50b7d4395ed44bc8338a703f729c0
b-duncs <4217@holbertonschool.com>
2022-08-02 21:14:34 -0700

    fixed betty

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -8,16 +8,16 @@
  * Return: nth node of dlistint_t
  */
 
- dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
- {
-   for (; index != 0; index--)
-   {
-     if (head == NULL)
-     {
-       return (NULL);
-     }
-     head = head->next;
-   }
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+		head = head->next;
+	}
 
-   return (head);
- }
+	return (head);
+}
