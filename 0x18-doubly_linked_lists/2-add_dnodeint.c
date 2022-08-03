28e093d193208e0fe55cd55ca9e8f4830805c6fc
b-duncs <4217@holbertonschool.com>
2022-08-02 21:05:59 -0700

    fixed betty

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -8,23 +8,23 @@
  * Return: return (0) is the required function signature
  */
 
- dlistint_t *add_dnodeint(dlistint_t **head, const int n)
- {
-   dlistint_t *new;
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
 
-   new = malloc(sizeof(dlistint_t));
-   if (new == NULL)
-   {
-     return (NULL);
-   }
-   new->n = n;
-   new->prev = NULL;
-   new->next = *head;
-   if (*head != NULL)
-   {
-     (*head)->prev = new;
-   }
-   *head = new;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
 
-   return (new);
- }
+	return (new);
+}
