24b0a97781b91696427ffce6a9e1dcb84c27a9ff
b-duncs <4217@holbertonschool.com>
2022-08-02 21:34:34 -0700

    fixed betty

--- 0x18-doubly_linked_lists/6-sum_dlistint.c
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -3,19 +3,20 @@
 /**
  * sum_dlistint - program startup
  * @head: pointer to dlistint_t list
-(* Description: returns the sum of all the data (n) of a dlistint_t linked list)?
+(* Description: returns the sum of all the data (n)
+ * of a dlistint_t linked list)?
  * Return: sum of all the data in dlistint_t
  */
 
- int sum_dlistint(dlistint_t *head)
- {
-   int sum = 0;
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
 
-   while (head)
-   {
-     sum += head->n;
-     head = head->next;
-   }
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-   return (sum);
- }
+	return (sum);
+}
