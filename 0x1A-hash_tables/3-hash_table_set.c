3778be0fff2743d73cf198560dafaf54a694e7c5
b-duncs <4217@holbertonschool.com>
2022-08-04 21:49:26 -0700

    fixed betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,36 +17,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int x;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
 		return (0);
-	}
 	duplicate = strdup(value);
 	if (duplicate == NULL)
-	{
 		return (0);
-	}
 	index = key_index((const unsigned char *)key, ht->size);
 	for (x = index; ht->array[x]; x++)
 	{
 		if (strcmp(ht->array[x]->key, key) == 0)
-		{
 			free(ht->array[x]->value);
 			ht->array[x]->value = duplicate;
 			return (1);
-		}
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
-	{
 		free(duplicate);
 		return (0);
-	}
 	new->key = strdup(key);
 	if (new->key == NULL)
-	{
 		free(new);
 		return (0);
-	}
 	new->value = duplicate;
 	new->next = ht->array[index];
 	ht->array[index] = new;
