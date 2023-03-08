5baec53cbd6faf7492ca83f6e6b46cca7e3b6377
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-03-07 19:43:28 -0600

    added files

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,74 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
+
 /**
- * print_arr - Print the elements of an array
+ * rec_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * @array: pointer to the first element of the array to print
- * @size: size of elements in the array
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-
-void print_arr(int *array, size_t size)
+int rec_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t half = size / 2;
+	size_t i;
 
-	printf("Searching in array: ");
-	while (i < size)
-	{
-		printf("%d, ", array[i]);
-		i++;
-	}
-	printf("%d", array[i]);
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
 	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
 }
+
 /**
- * advanced_binary - function that searches for a value in a sorted array
- * of integers using the Binary search algorithm
+ * advanced_binary - calls to rec_search to return
+ * the index of the number
  *
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- *
- * Return: return the index where value is located
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	int l = 0, r = size - 1;
+	int index;
 
-	if (!array)
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
 
-	return (b_search(array, l, r, size, value));
-}
-/**
- * b_search - function that searches for a value in a sorted array
- * of integers using the Binary search algorithm
- *
- * @array: pointer to the first element of the array to search in
- * @l: index of the first element of the array to search in
- * @r: index of the last element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- *
- * Return: return the index where value is located
- */
-int b_search(int *array, int l, int r, size_t size, int value)
-{
-	int p = 0;
-
-	if (l > r)
-		return (-1);
-
-	print_arr(&array[l], (r - l));
-	p = (l + r) / 2;
-
-	if (value == array[p] && array[p - 1] != value)
-		return (p);
-
-	if (value <= array[p])
-		r = p;
-	else
-		l = p + 1;
-
-	return (b_search(array, l, r, size, value));
+	return (index);
 }
